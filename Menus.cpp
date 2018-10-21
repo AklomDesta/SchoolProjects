f672ddbfdaea62273d1a20fbaae6e6ef945a16ad
aklom desta <chuna2811@gmail.com>
2018-10-21 11:48:46 -0700

    display menu for actor is done

--- Menus.cpp
+++ Menus.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "Menus.h"
-#include <conio.h>
+
 
 
 void Menus::display_main_menu(){
@@ -18,7 +18,6 @@ void Menus::display_main_menu(){
     {
         case 1:
             displayMenu_data_set_menu();
-            system("clear");
             break;
         case 2:
             cout<<"display updated data set menu is in progress..."<<endl;
@@ -45,7 +44,6 @@ void Menus::displayMenu_data_set_menu() {
     {
         case 1:
             displayMenu_actor_dataset_menu();
-            system("cls");
             break;
         case 2:
                 cout<<"Movie data set menu is in progress..."<<endl;
@@ -59,7 +57,7 @@ void Menus::displayMenu_data_set_menu() {
             exit(1);
 
     }
-    system("CLS");
+
 }
 void Menus::displayMenu_actor_dataset_menu(){
     int choose;
@@ -90,7 +88,7 @@ void Menus::displayMenu_actor_dataset_menu(){
 
             break;
         case 5:
-                cout<<"Sorting data set.."<<endl;
+                displayMenu_actor_sort_menu();
             break;
         case 6:
             displayMenu_actor_dataset_menu();
@@ -104,15 +102,15 @@ void Menus::displayMenu_actor_dataset_menu(){
             exit(1);
 
     }
-    system("CLS");
+
 }
 
 void Menus::displayMenu_actor_search_menu() {
     int choose;
     cout<<"********** WELCOME TO ACTOR DATA SET SEARCH MENU***********"<<endl;
     cout<<"Please choose a task"<<endl;
-    cout<<"1:- Partial search by.."<<endl;//menu
-    cout<<"2:- Complete search by.."<<endl;//menu
+    cout<<"1:- Partial search by.."<<endl;
+    cout<<"2:- Complete search by.."<<endl;
     cout<<"3:- Back to actor data set menu."<<endl;
     cout<<"4:- Back to data sets menu."<<endl;
     cout<<"5:- Back to main menu."<<endl;
@@ -120,10 +118,10 @@ void Menus::displayMenu_actor_search_menu() {
     cin>>choose;
     switch (choose){
         case 1:
-            cout<<"Partial search by menu..."<<endl;
+            cout<<"Partial search"<<endl;
             break;
         case 2:
-            cout<<"Complete search by ..menu"<<endl;//menu
+            cout<<"Complete search"<<endl;
             break;
         case 3:
             displayMenu_actor_dataset_menu();
@@ -140,7 +138,7 @@ void Menus::displayMenu_actor_search_menu() {
             exit(1);
 
     }
-    system("CLS");
+
 }
 
 void Menus::displayMenu_actor_sort_menu() {
@@ -158,6 +156,39 @@ void Menus::displayMenu_actor_sort_menu() {
     cout<<"9:- Back to main menu."<<endl;
     cout<<"10:- Exit the program."<<endl;
     cin>>choose;
+    switch (choose){
+        case 1:
+            cout<<"Sotring by word"<<endl;
+            break;
+        case 2:
+            cout<<"sorting by year"<<endl;
+            break;
+        case 3:
+            cout<<"sorting by winner"<<endl;
+            break;
+        case 4:
+            cout<<"sorting name"<<endl;
+            break;
+        case 5:
+            cout<<"sorting by file"<<endl;
+            break;
+        case 6:
+            displayMenu_actor_search_menu();
+            break;
+        case 7:
+            displayMenu_actor_dataset_menu();
+            break;
+        case 8:
+            displayMenu_data_set_menu();
+            break;
+        case 9:
+            display_main_menu();
+            break;
+        case 10:
+            exit(1);
+        default:
+            exit(1);
+    }
 
 }
 
